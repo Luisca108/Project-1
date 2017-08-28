769b22552f70d020a989e33d301c83086fe6b7c5
Luis Carlos <luiscarlos3106@gmail.com>
2017-08-28 14:23:41 +0200

    second commit

--- myGame.js
+++ myGame.js
@@ -8,23 +8,26 @@ var pig = document.getElementById('pig');
 
 var skeletonLeft = 0;
 var pigLeft = 0;
+var pigRight = 0;
 
 
 //skeleton move
 function anim(e) {
   // alert(e.keyCode);
   if(e.keyCode == 39) {
+    if (skeletonLeft< 1336.500 - 150) {
     skeletonLeft += 8;
     skeleton.style.left = skeletonLeft + "px";
-     if(skeletonLeft >= 900) {
-       skeletonLeft -=0;
-     }
+   }
+    $("#skeleton").css("transform", "rotateY(360deg)");
   }
-  if(e.keyCode == 37)
-  skeletonLeft -= 8;
-  skeleton.style.left = skeletonLeft + "px";
-   if(skeletonLeft <= 0) {
-    skeletonLeft += 2;
+
+  if(e.keyCode == 37) {
+    if (skeletonLeft > 0) {
+   skeletonLeft -= 8;
+   skeleton.style.left = skeletonLeft + "px";
+   }
+    $("#skeleton").css("transform", "rotateY(180deg)");
   }
 }
 
@@ -33,6 +36,8 @@ document.onkeydown = anim;
 //pig move
 var fps = 200;
 setInterval(function() {
-  pigLeft += 1;
-  pig.style.left = pigLeft + "px";
+  if (pigLeft< 1336.500 - 268) {
+    pigLeft += 1;
+    pig.style.left = pigLeft + "px";
+  }
 }, 1000/fps);
